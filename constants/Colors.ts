15307553460143c60812a21117859e791870a70e
Matheus Dias <hmatheus031@gmail.com>
2025-03-09 19:13:22 -0300

    feat: enhance color constants and improve theme color hook for better styling

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,40 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: {
+      default: "#11181C",
+      subtitle: "#687076",
+      title: "#000",
+      defaultSemiBold: "#000",
+      link: "#0a7ea4",
+    },
+    background: "#fff",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    secondaryTint: "#b5d8e3",
+    icon: "#687076",
+    tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
+    shadow: "#000",
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: {
+      default: "#ECEDEE",
+      subtitle: "#9BA1A6",
+      title: "#fff",
+      defaultSemiBold: "#fff",
+      link: "#0a7ea4",
+    },
+    background: "#151718",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    secondaryTint: "#b5d8e3",
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
+    shadow: "#000",
   },
 };
